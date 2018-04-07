754ece415fc97f04a4c8651099bfcc5b9b53029f
Flavio <35164223+ThePennywise@users.noreply.github.com>
2018-04-07 14:19:02 -0300

    Update ArgsValidator.java

--- src/io/github/pennywise/betest/utils/ArgsValidator.java
+++ src/io/github/pennywise/betest/utils/ArgsValidator.java
@@ -2,15 +2,15 @@ package io.github.pennywise.betest.utils;
 
 public class ArgsValidator {
 	
-	// Validar os argumentos passados
-	// O método é estático para facilitar o uso aleatório das classes
+	// Validate the args passed
+	// This method is static to easily be called.
 	public static boolean checkForInteger(String argument) {
 		try {
 			Integer.parseInt(argument);
-			// Se não deu nenhum erro, já pode retornar verdadeiro.
+			// If there is no errors, it's an integer.
 			return true;
 		} catch (NumberFormatException ex) {
-			// Se deu erro, o argumento passado não é um número inteiro, portanto, retorna falso.
+			// There is an error, It's not an integer.
 			return false;
 		}
 	}
